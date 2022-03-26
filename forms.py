df8a73627d0f7c01d0c3ec58eac7f960c694d7d9
StanislavUIT <klibashev.s@beldor.centr.by>
2022-03-26 19:01:18 +0300

    login  (doesnt work)

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField, SubmitField, FloatField, SelectField
+from wtforms import StringField, IntegerField, DateField, SubmitField, FloatField, SelectField, BooleanField
 from wtforms.validators import DataRequired
 
 class StudentForm(FlaskForm):
@@ -20,3 +20,9 @@ class StudentForm(FlaskForm):
 class SubjectForm(FlaskForm):
     name = StringField('Название предмета:* ', validators=[DataRequired()])
     submit = SubmitField('Добавить')
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя:', validators=[DataRequired()])
+    password = StringField('Пароль:', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, url_for
 from app import app, db, login
-from forms import StudentForm, SubjectForm
+from forms import StudentForm, SubjectForm, LoginForm
 from models import Subjects, Students, User
+from flask_login import current_user, login_user
 
 
 @login.user_loader
@@ -88,3 +89,18 @@ def delete_students(id):
 @app.errorhandler(404)
 def error404(error):
     return render_template('404.html'), 404
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data)
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for(login))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+
