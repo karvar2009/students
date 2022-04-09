d3007ab84104c449902a15c9da2bb9473cea6812
StanislavUIT <klibashev.s@beldor.centr.by>
2022-04-09 17:07:44 +0300

    register

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField, SubmitField, FloatField, SelectField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField, DateField, SubmitField, FloatField, SelectField, BooleanField, PasswordField
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Email
+from models import User
 
 class StudentForm(FlaskForm):
     name = StringField('Имя ученика:* ', validators=[DataRequired()])
@@ -26,3 +27,27 @@ class LoginForm(FlaskForm):
     password = StringField('Пароль:', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Имя:', validators=[DataRequired()])
+    password = PasswordField('Пароль:', validators=[DataRequired()])
+    password_rep = PasswordField('Пароль:', validators=[DataRequired(), EqualTo('password')])
+    email = StringField('Электронная почта:', validators=[DataRequired()])
+    sec_key = StringField('Секретный ключ (запросите у школы):', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Это имя пользователя занято')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Эта электронная почта занята')
+
+    def validate_keyy(self, keyy):
+        keys = keyy.query.filter_by(keyy=keyy.data).first()
+        if keys is None:
+            raise ValidationError('Такого ключа нет!!!')
--- routes.py
+++ routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect, url_for
 from app import app, db, login
-from forms import StudentForm, SubjectForm, LoginForm
+from forms import StudentForm, SubjectForm, LoginForm, RegisterForm
 from models import Subjects, Students, User
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user, login_required
 
 
 @login.user_loader
@@ -15,6 +15,7 @@ def index():
 
 
 @app.route('/add_student', methods=['GET', 'POST'])
+@login_required
 def add_student():
     form = StudentForm()
     form.subject.choices = [(subject.id, subject.name) for subject in Subjects.query.order_by(Subjects.name).all()]
@@ -96,11 +97,27 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data)
+        user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             return redirect(url_for(login))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
